c97074a42663f8bd02eae7a5ea728a211c8749f4
hiihtoliitto <jussi.rintala@gmail.com>
2017-02-15 13:22:18 +0200

    avaus-luokka

--- src/main/java/tikape/runko/domain/Avaus.java
+++ src/main/java/tikape/runko/domain/Avaus.java
@@ -5,10 +5,52 @@
  */
 package tikape.runko.domain;
 
+import java.util.Calendar;
+
 /**
  *
- * @author o
+ * @author oolli
  */
 public class Avaus {
-    
+
+    private final int avaus_id;
+    private final int alueviittaus;
+    private final String otsikko;
+    private final String sisalto;
+    private final String nimimerkki;
+    private final Calendar aikaleima;
+
+    public Avaus(int avaus_id, int alueviittaus, String otsikko, String sisalto, String nimimerkki, Calendar aikaleima) {
+        this.avaus_id = avaus_id;
+        this.alueviittaus = alueviittaus;
+        this.nimimerkki = nimimerkki;
+        this.sisalto = sisalto;
+        this.otsikko = otsikko;
+        this.aikaleima = aikaleima;
+    }
+
+    public int getAvaus_id() {
+        return this.avaus_id;
+    }
+
+    public int getAlueviittaus() {
+        return this.avaus_id;
+    }
+
+    public String getOtsikko() {
+        return this.otsikko;
+    }
+
+    public String getNimimerkki() {
+        return this.nimimerkki;
+    }
+
+    public String getSisalto() {
+        return this.otsikko;
+    }
+
+    public Calendar getAikaleima() {
+        return this.aikaleima;
+    }
+
 }
